67ade1b2fdc2933d53b783ee9ea2a9cd7bef54c8
Alberto <100439496@alumnos.uc3m.es>
2023-12-05 22:08:18 +0100

    changes

--- works_scrapper.py
+++ works_scrapper.py
@@ -1,23 +1,15 @@
 import asyncio
 import warnings
-from selenium.webdriver.common.by import By
 import aiohttp
-from selenium.common.exceptions import NoSuchElementException
 import json
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+from urllib.request import urlopen
+from pathlib import Path
 
 
 warnings.simplefilter(action="ignore", category=FutureWarning)
 import pandas as pd
 
 CSV_FILE_PATH = "cs_works_dataset.csv"
-
-
-# This code takes in a work, and extracts a number of features from it
-# including the title, year, language, number of citations, type, whether
-# it has been retracted, institution type, countries, and concepts.
-# These features are then appended to a CSV file.
 
 
 async def get_work(work):
@@ -31,8 +23,8 @@ async def get_work(work):
             "type": str,
             "retracted": bool,
             "institution_type": str,
-            "countries": list,
-            "concepts": list,
+            "countries": [],
+            "concepts": [],
         }
     )
 
@@ -40,13 +32,15 @@ async def get_work(work):
     institution_types = []
 
     for authorship in work["authorships"]:
+        if "countries" in authorship:
+            countries.extend(authorship["countries"])
         for institution in authorship["institutions"]:
-            countries.append(
-                institution["country"] if "country" in institution else None
-            )
             institution_types.append(
                 institution["type"] if "type" in institution else None
             )
+
+    if len(countries) <= 1:
+        return
 
     df = df.append(
         pd.Series(
@@ -54,12 +48,12 @@ async def get_work(work):
                 work["id"],
                 work["title"],
                 work["publication_year"],
-                work["language"] if "landuage" in work else None,
+                work["language"] if "language" in work else None,
                 work["cited_by_count"] if "cited_by_count" in work else None,
                 work["type"] if "type" in work else None,
-                work["retracted"],
+                work["is_retracted"],
                 institution_types if institution_types else None,
-                countries if countries else None,
+                countries,
                 [concept["display_name"] for concept in work["concepts"]],
             ],
             index=df.columns,
@@ -67,9 +61,6 @@ async def get_work(work):
         ignore_index=True,
     )
     df.to_csv(CSV_FILE_PATH, mode="a", index=False, header=False)
-
-
-# The code takes the JSON data from the API and parses through it to extract key information such as the title, year, language, number of citations, and whether the paper was retracted. It then saves this information to a CSV file and stores the data in a dataframe. It also saves the cursor value to a text file so that the code can continue from where it left off if there is an error or the code needs to be stopped. The code also checks for errors in the JSON data and prints a message if there is an error. If there are no errors, it moves on to the next paper.
 
 
 async def main():
@@ -84,26 +75,26 @@ async def main():
             "type": str,
             "retracted": bool,
             "institution_type": str,
-            "countries": list,
-            "concepts": list,
-        },
-        index=[0],
+            "countries": [],
+            "concepts": [],
+        }
     )
-    header.to_csv(CSV_FILE_PATH, index=False)
-    options = Options()
-
-    driver = webdriver.Chrome(options=options)
+    csv_path = Path(CSV_FILE_PATH)
+    if not csv_path.is_file():
+        header.to_csv(CSV_FILE_PATH, index=False)
 
     next_cursor = "*"
-    with aiohttp.ClientSession(connector=aiohttp.TCPConnector(ssl=False)) as session:
+    async with aiohttp.ClientSession(
+        connector=aiohttp.TCPConnector(ssl=False)
+    ) as session:
         while next_cursor:
             # Change URL if local version is available
-            url = f"https://api.openalex.org/works?filter=concept.id:C41008148,publication_year:%3E1989,publication_year:%3C2023&sort=cited_by_count:desc&per-page=200&cursor={next_cursor}"
-            driver.get(url)
+            url = f"https://api.openalex.org/works?filter=concept.id:C41008148,institutions_distinct_count:%3E1,publication_year:%3E1989,publication_year:%3C2023&sort=cited_by_count:desc&per-page=200&cursor={next_cursor}"
             try:
-                content = driver.find_element(By.TAG_NAME, "pre").text
-            except NoSuchElementException:
-                print("Error finding JSON data on page:", url)
+                with urlopen(url) as response:
+                    content = response.read().decode("utf-8")
+            except Exception as e:
+                print(f"Error downloading content from {url}: {e}")
                 continue
             try:
                 parsed_json = json.loads(content)
